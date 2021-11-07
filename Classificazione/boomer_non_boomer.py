462d21b351e31c24038546ac87a83bdab19bcc83
lorenzo.stacchio <lorenzo.stacchio.dev@gmail.com>
2021-11-07 16:08:45 +0100

    End boomer vs non boomer notebook

--- Classificazione/boomer_non_boomer.py
+++ Classificazione/boomer_non_boomer.py
@@ -3,12 +3,13 @@ import pandas as pd
 import json
 import numpy as np
 # Create new features
+import names
 
 def create_new_features(dataframe: pd.DataFrame, features_name: str, range_of_values: (int, int), n_values: int):
-    print(range_of_values[0], range_of_values[1])
+    # print(range_of_values[0], range_of_values[1])
     mu = (int(range_of_values[1]+1)+int(range_of_values[0]))//2
     sigma = (int(range_of_values[1]+1) - int(range_of_values[0]))//5
-    print(features_name, mu,sigma)
+    # print(features_name, mu,sigma)
     list_of_values = np.abs(np.random.normal(mu, sigma, n_values))#.astype(int)
     dataframe[features_name] = list_of_values
     return dataframe
@@ -17,7 +18,7 @@ def create_new_features(dataframe: pd.DataFrame, features_name: str, range_of_va
 if __name__ == '__main__':
     n_sample = 5000
     features_dict = json.load(open('range_of_values.json',))
-    print(features_dict)
+    # print(features_dict)
     #features = ["numero di foto di buongiorno", "numero di like per foto", "numero di commenti per foto"]
     df_boomer = pd.DataFrame()
     df_non_boomer = pd.DataFrame()
@@ -31,6 +32,8 @@ if __name__ == '__main__':
     df_non_boomer["boomer"] = [0]*n_sample
 
     final_df = df_boomer.append(df_non_boomer,ignore_index=True)
-    final_df.to_csv("boomer_non_boomer.csv")
+    final_df["name"] = [names.get_first_name() for i in range(len(final_df))]
+    final_df["surname"] = [names.get_last_name() for i in range(len(final_df))]
+    final_df.to_csv("boomer_non_boomer_names.csv", index=False)
     #print(df_boomer,df_non_boomer)
 
